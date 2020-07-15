d294763b56aa8b80ac1aa0f40ddbbc6508ef0199
charliemedina <charliemedina1989@gmail.com>
2020-07-15 10:46:50 +0200

    Add Swagger

--- WebAPI/WebAPI/Startup.cs
+++ WebAPI/WebAPI/Startup.cs
@@ -6,9 +6,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using WebAPI.Models;
 using WebAPI.Services;
 using WebAPI.Validators;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebAPI
 {
@@ -33,6 +35,11 @@ namespace WebAPI
             options.UseSqlServer(Configuration.GetConnectionString("DevDatabase")));
 
             services.AddAutoMapper(typeof(Startup));
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Users API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +49,17 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
